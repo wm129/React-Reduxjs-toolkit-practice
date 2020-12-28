66229a5915db173df1683dd8f0d270f89eff235c
wm129 <sekaimanyuucamp@gmail.com>
2020-12-28 23:28:41 +0900

    change

--- toolkitapp/src/Stores/user.js
+++ toolkitapp/src/Stores/user.js
@@ -1,58 +1,57 @@
-import {createSlice} from '@reduxjs/toolkit';
+import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-    name: "Wakamori",
-    age: 24,
-    skills: ['js', 'react', 'firebase'],
-    imageUrl: '',
+  name: 'koga',
+  age: 10,
+  skills: ['js', 'react', 'firebase'],
+  imageUrl: '',
 };
 
 const slice = createSlice({
-    name: 'user',
-    initialState,
-    reducers: {
-        changeName: (state, action) => {
-            //新しいステート
-            return {
-                ...state,
-                name: action.payload,
-            }
-        },
-        berthday: (state) => {
-            return {
-                ...state,
-                age: state.age + 1,
-            }
-        },
-        addSkills: (state = [], action) => {
-            return {
-                ...state,
-                skills: action.payload,
-            };
-        },
-        changeImageUrl: (state, action) => {
-            return {
-                ...state,
-                imageUrl: action.payload,
-            };
-        },
+  name: 'user',
+  initialState,
+  reducers: {
+    // state: 現在のstate
+    // action: action.payloadの中にaction関数の引数が入る
+    changeName: (state, action) => {
+      // 新しいstateがreturnされる
+      return {
+        ...state,
+        name: action.payload,
+      };
     },
+    berthday: (state) => {
+      return {
+        ...state,
+        age: state.age + 1,
+      };
+    },
+    // 書いておく
+    changeImageUrl: (state, action) => {
+      return {
+        ...state,
+        imageUrl: action.payload,
+      };
+    },
+  },
 });
+
+// 追加
 const { changeImageUrl } = slice.actions;
 
-export const fetchImage = (imageUrl) => {
-    return async (dispatch) => {
-        try {
-            const response = await fetch('https://dog.ceo/api/breeds/image/random')
-            const data = await response.json();
-            dispatch(changeImageUrl(data.message));
-        } catch (error) {
-            console.log(error);
-        }
-    };
+// 追加
+export const fetchImage = () => {
+  return async (dispatch) => {
+    try {
+      const response = await fetch('https://dog.ceo/api/breeds/image/random');
+      const data = await response.json();
+      dispatch(changeImageUrl(data.message));
+    } catch (error) {
+      console.log(error);
+    }
+  };
 };
 
+export const { changeName, berthday } = slice.actions;
 
-
-export const { changeName, berthday, addSkills } = slice.actions
 export default slice.reducer;
