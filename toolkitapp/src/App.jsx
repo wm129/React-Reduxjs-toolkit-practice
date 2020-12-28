82b29e5b2b1215e0916ed2c247d6310027121e03
wm129 <sekaimanyuucamp@gmail.com>
2020-12-28 23:20:05 +0900

    change

--- toolkitapp/src/App.jsx
+++ toolkitapp/src/App.jsx
@@ -1,45 +1,37 @@
-import React,{ useState } from 'react';
+import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-
-import { changeName,berthday, fetchImage, addSkills } from './Stores/user';
+// 追加
+import { changeName, berthday, fetchImage } from './stores/user';
 
 import Counter from './Counter';
+
 const App = () => {
-    const dispatch = useDispatch();
-    const user = useSelector((state) => state.user);
-    const [isShow, setIsShow] = useState(true);
-    const [text, setText] = useState('');
-    const [subtext, setsubtext] = useState([]);
+  const dispach = useDispatch();
+  const user = useSelector((state) => state.user);
+  const [isShow, setIsShow] = useState(true);
+  const [text, setText] = useState('');
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        //textがaction.payloadにあたる。
-        dispatch(changeName(text));
-    };
-
-    const handleChange = (e) => {
-        e.preventDefault();
-        //subtextがaction.payloadにあたる。
-        dispatch(addSkills(subtext));
-    };
-
-    return(
-        <>
-        <form onSubmit={handleSubmit}>
-            <input value={text} onChange={(e) => setText(e.target.value)} />
-            <button>Change</button>
-        </form>
-        <button onClick={() =>dispatch(berthday())}>誕生日</button>
-        <form onSubmit={handleChange}>
-            <input value={text} onChange={(e) => setsubtext(e.target.value)} />
-            <button>Let's add Skills</button>
-        </form>
-        <button onClick={() => fetchImage()}>画像取得</button>
-        <h2>ユーザー名：{user.name}({user.age})</h2>
-        <button onClick={() => setIsShow(!isShow)}>show</button>
-         {isShow && <Counter />}
-        </>
-    );
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // textがaction.payloadにあたる
+    dispach(changeName(text));
+  };
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <input value={text} onChange={(e) => setText(e.target.value)} />
+        <button>変更</button>
+      </form>
+      <button onClick={() => dispach(berthday())}>誕生日</button>
+      {/* 追加 */}
+      <button onClick={() => fetchImage()}>画像取得</button>
+      <h2>
+        ユーザー名: {user.name}({user.age})
+      </h2>
+      <button onClick={() => setIsShow(!isShow)}>show</button>
+      {isShow && <Counter />}
+    </>
+  );
 };
-
 export default App;
+ 
